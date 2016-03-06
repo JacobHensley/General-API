1966c8ab149daad5adc16f5991557d108563478f
JacobHensley <blockmaster007+GitHub@gmail.com>
2016-03-05 20:04:00 -0600

    Java Class 3/5/2016

--- src/Graphics/Misc/Screen.java
+++ src/Graphics/Misc/Screen.java
@@ -36,7 +36,7 @@ public class Screen {
 					continue;
 				
 				pixels[xo + yo * this.width] = color;
-			}	
+			}
 		}
 		
 		for (int xl = 0; xl < 2;xl++) {
@@ -90,19 +90,77 @@ public class Screen {
 				if (color == 0xFFFF00FF)
 					continue;
 				
-				int r = (color >> 16) & 0xff;
-				int g = (color >> 8) & 0xff;
-				int b = (color) & 0xff;
+				pixels[xo + yo * this.width] = color;
+			}	
+		}
+	}
+	
+	public void drawTexture(Texture texture, int x, int y, float angle) {
+		drawCalls++;
+		double r = (Math.PI / 180) * angle;
+		
+		for (int yy = 0; yy < texture.getHeight(); yy++) {
+			int yo = y + yy;
+			if (yo < 0 || yo >= this.height)
+				continue;
+			for (int xx = 0; xx < texture.getWidth(); xx++) {
+				int xo = x + xx;
+				if (xo < 0 || xo >= this.width)
+					continue;
+				int[] rpixels = rotate(texture.getPixels(), texture.getHeight(), texture.getWidth(), r);
 				
-				if (r > 255) r = 255;
-				if (g > 255) g = 255;
-				if (b > 255) b = 255;
-				color = r << 16 | g << 8 | b;
+				int color = rpixels[xx + yy * texture.getWidth()];
+				if (color == 0xFFFF00FF)
+					continue;
 				
 				pixels[xo + yo * this.width] = color;
 			}	
 		}
+	}
+	
+	private int[] rotate(int[] pixels, int width, int height, double angle) {
+		int[] result = new int[width * height];
+		double nx_x = rotX(-angle, 1.0, 0.0);
+		double nx_y = rotY(-angle, 1.0, 0.0);
+		double ny_x = rotX(-angle, 0.0, 1.0);
+		double ny_y = rotY(-angle, 0.0, 1.0);
 		
+		double x0 = rotX(-angle, -width / 2.0, -height / 2.0) + width / 2.0;
+		double y0 = rotY(-angle, -width / 2.0, -height / 2.0) + height / 2.0;
+		
+		for (int y = 0;y < height;y++) {
+			double x1 = x0;
+			double y1 = y0; 
+			for (int x = 0;x < width;x++) {
+				int xx = (int) x1;
+				int yy = (int) y1;
+				int color;
+				if (xx < 0 || xx >= width || yy < 0 || yy >= height) 
+					color = 0xFFFF00FF;
+				else
+					color = pixels[xx + yy * width];
+				
+				result[x + y * width] = color;
+				x1 += nx_x;
+				y1 += nx_y;
+			}
+			x0 += ny_x;
+			y0 += ny_y;
+		}
+
+		return result;
+	}
+	
+	private double rotX(double angle, double x, double y) {
+		double cos = Math.cos(angle);
+		double sin = Math.sin(angle);
+		return x * cos + y * -sin;
+	}
+	
+	private double rotY(double angle, double x, double y) {
+		double cos = Math.cos(angle);
+		double sin = Math.sin(angle);
+		return x * sin + y * cos;
 	}
 	
 	public void drawString(String string, int x, int y, int z, Font font, Color color) {
